ef602d19f369a44bd208433a679fc447e6220936
sandra <sandraxmendoza@gmail.com>
2022-06-07 20:08:11 -0700

    changes to HTML sections

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,12 @@
-// curl --request POST \
-// --url https://beta3.api.climatiq.io/estimate \
-// --header 'Authorization: Bearer 7CVW88CEXD49DFKK71F9072RPEYH' \
-var data = {
-        "emission_factor": "electricity-energy_source_grid_mix",
-        "parameters":
-            {
-            "energy": 4200,
-            "energy_unit": "kWh"
-            }
-         };
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com',
+		'X-RapidAPI-Key': '2b55785d9emsh128efc9a2b8a7e8p11212ajsn4abb761087ca'
+	}
+};
 
-// Endpoint
-var endpoint = "https://beta3.api.climatiq.io/estimate";
-
-//Fetch
-fetch(endpoint, {
-    method: 'POST',
-    headers: {
-        Authorization: 'Bearer 7CVW88CEXD49DFKK71F9072RPEYH',
-    },
-    body: JSON.stringify(data)
-}).then(function(response) {
-    return response.json()
-}).then(function(data) {
-//Function to handle response
-    console.log(data)
-})
-
-
-
+fetch('https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel?distance=100&vehicle=SmallDieselCar', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));

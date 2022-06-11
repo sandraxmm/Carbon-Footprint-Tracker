c0d7ef77f2862e6736eeeccd30edf58745d3c391
sandra <sandraxmendoza@gmail.com>
2022-06-11 10:36:43 -0700

    added carbon calculator code

--- Assets/script.js
+++ Assets/script.js
@@ -1,56 +1,42 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com',
-		'X-RapidAPI-Key': '2b55785d9emsh128efc9a2b8a7e8p11212ajsn4abb761087ca'
-	}
-};
+//Carbon Footprint Calculator
+var button = document.querySelector("#submit-carbonfootprint")
+var carMileage = document.querySelector("#mileage")
+var carTypeInput = document.querySelector("#vehicle")
+var carbonFootprint = document.querySelector("#results");
+var carbonAPI = 'https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel?distance=100&vehicle=SmallDieselCar'
+var apiKey = '2b55785d9emsh128efc9a2b8a7e8p11212ajsn4abb761087ca'
 
-fetch('https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel?distance=100&vehicle=SmallDieselCar', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-// curl --request POST \
-// --url https://beta3.api.climatiq.io/estimate \
-// --header 'Authorization: Bearer 7CVW88CEXD49DFKK71F9072RPEYH' \
-var data = {
-        "emission_factor": "electricity-energy_source_grid_mix",
-        "parameters":
-            {
-            "energy": 4200,
-            "energy_unit": "kWh"
-            }
-         };
+function carbonFootprintAsk(event) {
+    event.preventDefault()
+    var distance = carMileage.value;
+    var carType = carTypeInput.value;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': apiKey,
+            'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com'
+        }
+    };
+    var URI = 'https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel?distance='+ distance +'&vehicle=' + carType;
+    fetch(URI, options)
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data)
+    })
+}
+//event listener for submit button
+button.addEventListener('click',carbonFootprintAsk);
 
-// Endpoint
-var endpoint = "https://beta3.api.climatiq.io/estimate";
+//append results to html - code NOT WORKING
+carbonFootprint.append(carbonFootprintAsk);
 
-//Fetch
-fetch(endpoint, {
-    method: 'POST',
-    headers: {
-        Authorization: 'Bearer 7CVW88CEXD49DFKK71F9072RPEYH',
-    },
-    body: JSON.stringify(data)
-}).then(function(response) {
-    console.log(response)
-    return response.json()
-    
-}).then(function(data) {
-//Function to handle response
-    console.log(data)
-
-    //saving into local storage jm
-    sendToLocalStor(data);
-})
-
+//keep this var?
 var prevCal = document.querySelector("#prevCal")
 
 //event listener added jm
 prevCal.addEventListener("click", function(){
 console.log(prevCal)
-// var  
-
 });
 
 function sendToLocalStor(data) {
